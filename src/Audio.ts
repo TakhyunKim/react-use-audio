cf3f0a99f42af076f6ccda92328bc9d241bdf9e1
Takhyun Kim <kimkih1218@gmail.com>
2023-06-29 00:44:09 +0900

    feat: add AudioController class and class variable

---
+++ src/Audio.ts
@@ -0,0 +1,39 @@
+export type AudioData = {
+  isPause: boolean;
+  isPlaying: boolean;
+  name: string;
+  audioBufferSourceNode: AudioBufferSourceNode;
+};
+
+export type AudioSnapshot = {
+  data: AudioData;
+  play: () => void;
+  stop: () => void;
+  pause: () => void;
+};
+
+export class AudioController {
+  private listeners: Set<() => void>;
+  private audioContext: AudioContext;
+  private audioBuffer: AudioBuffer | null;
+  private audioBufferSourceNode: AudioBufferSourceNode;
+  private snapshot: AudioSnapshot;
+
+  constructor() {
+    this.listeners = new Set();
+    this.audioContext = new AudioContext();
+    this.audioBufferSourceNode = this.audioContext.createBufferSource();
+    this.audioBuffer = null;
+    this.snapshot = {
+      data: {
+        isPlaying: false,
+        isPause: false,
+        name: "",
+        audioBufferSourceNode: this.audioBufferSourceNode,
+      },
+      play: () => console.log("play"),
+      stop: () => console.log("stop"),
+      pause: () => console.log("pause"),
+    };
+  }
+}
