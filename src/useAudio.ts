f7cd2c6f24ac64e014fb489ee886e1b4df4c46f7
Takhyun Kim <kimkih1218@gmail.com>
2023-07-05 02:00:15 +0900

    feat: add useAudio hook logic using useSyncExternalStore

--- src/useAudio.ts
+++ src/useAudio.ts
@@ -1,3 +1,25 @@
-export const useAudio = () => {
-  console.log("use audio");
+import { useRef, useCallback, useSyncExternalStore } from "react";
+
+import { AudioController } from "./Audio";
+
+export const useAudio = (audio: string) => {
+  const audioControllerRef = useRef<AudioController | null>(null);
+
+  if (audioControllerRef.current === null) {
+    audioControllerRef.current = new AudioController();
+  }
+
+  const subscribe = useCallback(
+    (listener: () => void) =>
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      audioControllerRef!.current!.subscribe(listener, audio),
+    [audio]
+  );
+
+  const audioInfo = useSyncExternalStore(
+    subscribe,
+    audioControllerRef?.current.getSnapshot
+  );
+
+  return audioInfo;
 };
